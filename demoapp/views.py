afdc8daaee28c2fbfd3a8d9ba6ab20afac58accf
mumble <andreapauline.deguzman@gmail.com>
2024-02-07 12:07:24 +0800

    views, models, schema, urls-routes

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
 
 # Create your views here.
 
 def index(request):
   # return HttpResponse("hello World")
   return render(request, "home.html")
+
+def todos(request):
+  items = TodoItem.objects.all()
+  return render(request, "todos.html", {"todos": items })
